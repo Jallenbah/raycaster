d4bfed470e1ad05eddabcd6f8dffba16907968a5
Jallen <111014323+Jallenbah@users.noreply.github.com>
2022-08-24 21:34:27 +0100

    Renamed/removed pixelwindow sample code

---
+++ src/App/RaycasterAppManager.cs
@@ -0,0 +1,22 @@
+﻿using PixelWindowSystem;
+using SFML.Graphics;
+
+internal class RaycasterAppManager : IPixelWindowAppManager
+{
+    public void OnLoad(RenderWindow renderWindow)
+    {
+    }
+
+    public void Update(double frameTime)
+    {
+    }
+
+    public void FixedUpdate(double timeStep)
+    {
+    }
+
+    public void Render(PixelData pixelData, double frameTime)
+    {
+        pixelData.Clear();
+    }
+}
