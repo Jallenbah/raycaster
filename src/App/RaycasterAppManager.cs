eb03f9fe28fb1b1e3692cff0e19dba1ca943e7d7
Jallen <111014323+Jallenbah@users.noreply.github.com>
2022-08-28 17:09:38 +0100

    Ray vector calculation & debug display

--- src/App/RaycasterAppManager.cs
+++ src/App/RaycasterAppManager.cs
@@ -5,7 +5,20 @@ using VectorMaths;
 
 internal class RaycasterAppManager : IPixelWindowAppManager
 {
-    private Vector2 _secondHandDirection = new Vector2(0, -1);
+    private Vector2 _cameraPos;
+    private Vector2 _cameraDirection;
+    private Vector2 _cameraPlane;
+
+    private float _fovDegrees = 90;
+
+    public RaycasterAppManager()
+    {
+        _cameraPos = new Vector2(8, 8);
+        _cameraDirection = Vector2.Normalize(new Vector2(1, 1));
+        _cameraPlane = _cameraDirection.Rotate(AngleHelper.RadiansToDegrees(90));
+
+        _cameraPlane *= MathF.Tan(AngleHelper.DegreesToRadians(_fovDegrees/2));
+    }
 
     public void OnLoad(RenderWindow renderWindow)
     {
@@ -17,19 +30,50 @@ internal class RaycasterAppManager : IPixelWindowAppManager
 
     public void FixedUpdate(float timeStep)
     {
-        var stepsPerMinute = (1000 / timeStep) * 60;
-        _secondHandDirection = _secondHandDirection.Rotate(AngleHelper.DegreesToRadians(360/stepsPerMinute));
     }
 
     public void Render(PixelData pixelData, float frameTime)
     {
-        const uint offset = 30;
-        const uint secondHandLength = 25;
-
-        var secondHandEnd = (_secondHandDirection * secondHandLength) + new Vector2(offset, offset);
-
         pixelData.Clear();
-        pixelData[offset, offset] = (255, 0, 0);
-        pixelData[(uint)secondHandEnd.X, (uint)secondHandEnd.Y] = (0, 255, 0);
+
+        var mult = pixelData.Width / 5;
+        (uint x, uint y) midPos = (pixelData.Width / 2, pixelData.Height / 2);
+        pixelData[midPos.x, midPos.y] = (0, 255, 0);
+
+        for (uint x = 0; x < pixelData.Width; x++)
+        {
+            var xAsViewportCoord = PixelToViewportCoord(x, pixelData.Width);
+            var rayVector = Vector2.Normalize((xAsViewportCoord * _cameraPlane) + _cameraDirection);
+
+            var test = rayVector * mult;
+
+            pixelData[(uint)(midPos.x + MathF.Round(test.X)), (uint)(midPos.y + MathF.Round(test.Y))] = (255, 0, 0);
+        }
     }
+
+    private float PixelToViewportCoord(uint pixelCoord, uint pixelScreenLength) => (pixelCoord - (pixelScreenLength / 2f)) / (pixelScreenLength / 2f);
+
+    private Vector2? CastRay(Vector2 cameraPos, Vector2 direction)
+    {
+        return null;
+    }
+
+    private readonly int[,] _map = {
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,1,1,0,0,0,0,1,0,1,0,1,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
 }
